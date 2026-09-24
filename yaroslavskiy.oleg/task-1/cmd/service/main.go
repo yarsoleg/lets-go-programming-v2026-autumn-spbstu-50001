fc68f459874e728b74b5bd05fd55195be168a400
Dragon Legend <yars.oleg@mail.ru>
2026-09-24 16:29:28 +0300

    [TASK-1] Initial solution

---
+++ yaroslavskiy.oleg/task-1/cmd/service/main.go
@@ -0,0 +1,55 @@
+
+
+// Yaroslavsky Oleg 5130904 / 50001
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		operand1, operand2 int
+		operation          string
+	)
+
+	_, err := fmt.Scan(&operand1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scan(&operand2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+	
+	switch operation {
+	case "+":
+		fmt.Println(operand1 + operand2)
+	case "-":
+		fmt.Println(operand1 - operand2)
+	case "*":
+		fmt.Println(operand1 * operand2)
+	case "/":
+		if operand2 == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(operand1 / operand2)
+		}
+	default:
+		fmt.Println("Invalid operation")
+	}
+
+
+}
+
+
---
+++ yaroslavskiy.oleg/task-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/yarsoleg/task-1
+
+go 1.22
